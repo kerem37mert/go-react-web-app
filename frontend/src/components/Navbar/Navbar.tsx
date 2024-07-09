e24c38e71f0537d5561302fb2a4c12122de45a91
Kerem Mert <kerem37mert@yahoo.com>
2024-07-09 20:35:00 +0300

    Theme test

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../../contexts/ThemeContext.tsx";
+
+// Style
+import "./Navbar.style.css";
 
 const Navbar: React.FC = () => {
+
+    const { theme } = useContext(ThemeContext)
+
     return(
-        <div>
+        <div className={`navbar-container-${theme}`}>
             Navbar
         </div>
     );
