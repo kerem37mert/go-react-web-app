6ea6fbb1204ef656f80a776fc114bc96cb3675b5
Kerem Mert <kerem37mert@yahoo.com>
2024-07-11 09:02:45 +0300

    themecontext updated

--- frontend/src/contexts/ThemeContext.tsx
+++ frontend/src/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, ReactNode, useState} from "react";
+import React, {createContext, ReactNode, useEffect, useState} from "react";
 
 const ThemeContext= createContext();
 
@@ -8,7 +8,15 @@ interface ThemeProviderProps {
 
 const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
 
-    const [theme, setTheme] = useState<"light" | "dark">("dark")
+    const [theme, setTheme] = useState<"light" | "dark">("light");
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme") as "light" | "dark" | null;
+        if(storedTheme)
+            setTheme(storedTheme)
+        else
+            setTheme("light")
+    }, []);
 
     return(
         <ThemeContext.Provider value={{ theme, setTheme }}>
